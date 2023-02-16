2f483048a1a9870c7923b5c995830c4bacf1ebf8
Prashant Mishra <prashantmishra111@outlook.com>
2023-02-16 12:02:22 +0530

    1st commit

---
+++ ctax/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import LandingPage from './component/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Hello Coder!!!
+       <LandingPage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
